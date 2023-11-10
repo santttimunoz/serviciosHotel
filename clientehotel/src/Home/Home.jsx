318d37cb6308a7d4ec8f124d1b392216b1e14594
Diego velez <diegoalejandrovelezlopez@gmail.com>
2023-11-10 08:47:57 -0500

    agregar cliente al servidor

---
+++ clientehotel/src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import './Home.css'
+import { Form } from "../Form/Form.jsx";
+
+export function Home(){
+    return(
+        <>
+        <div className="banner">
+            <Form/>
+        </div>
+        </>
+    )
+}
